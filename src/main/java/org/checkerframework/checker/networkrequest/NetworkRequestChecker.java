ec8ca87878ddf6368ecfdc3952c773e72031ba0c
Werner Dietl <wdietl@gmail.com>
2023-07-26 06:31:50 -0400

    Update to errorprone 2.20.0

--- src/main/java/org/checkerframework/checker/networkrequest/NetworkRequestChecker.java
+++ src/main/java/org/checkerframework/checker/networkrequest/NetworkRequestChecker.java
@@ -8,6 +8,7 @@ import java.util.LinkedHashSet;
 /** This is the entry point for pluggable type-checking. */
 public class NetworkRequestChecker extends BaseTypeChecker {
 
+    @SuppressWarnings("NonApiType") // Until signature is adapted in the superclass
     @Override
     protected LinkedHashSet<Class<? extends BaseTypeChecker>> getImmediateSubcheckerClasses() {
         LinkedHashSet<Class<? extends BaseTypeChecker>> checkers =
